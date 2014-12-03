f499937f37bc75f07989ecc6fb32c884f8d8cf07
Patrick Riggs <riggspc@gmail.com>
2014-12-02 18:16:26 -0800

    merge sort skeleton

---
+++ merge sort/solution.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+using namespace std;
+
+void mergesort(vector<int>);
+vector<int> merge(vector<int>, vector<int>);
+
+int main(int argc, char const *argv[])
+{
+	vector<int> arr = {10, 3, -1, 12, 0};
+	mergesort(arr);
+	return 0;
+}
+
+void mergesort(vector<int> arr) {
+
+
+
+
+
+
+}
+
+// merges two sorted arrays
+vector<int> merge(vector<int>, vector<int>) {
+
+
+
+
+
+}
